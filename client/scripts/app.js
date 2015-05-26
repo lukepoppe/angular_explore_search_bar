7605d16ebb6bf612669dc4c31afe3be2ae022045
lukepoppe <lukepoppe@hotmail.com>
2015-05-26 11:07:27 -0500

    first

--- client/scripts/app.js
+++ client/scripts/app.js
@@ -2,10 +2,40 @@ var myApp = angular.module('myApp',['ngAnimate']);
 
 
 
-myApp.controller('myCtrl',["$scope", function($scope){
-        $scope.bacon=['BLT Sandwich','Wrapped around Chicken/Turkey Breast','By the Plateful','Chocolate covered Bacon' ,'Maple Bacon Cupcakes','Double Bacon Butter-Burger','Bacon Stuffed Peppers' ,'Barbecue-baked Beans with Bacon','Bacon and Eggs','Bacon, Egg and Cheese Biscuits','Bacon and Waffles','Corn and Bacon Chowder ','Barbecue Chicken Pizza with Onions and BACON!'];
-        }
+myApp.controller('myCtrl',['$scope', function($scope){
+        $scope.contact=[{name:"Cody-", email:"cody@fake.com"},
+            {name:"Mary-", email:"mary@fake.com"},
+            {name:"Brian-", email:"Brian@fake.com"},
+            {name:"Alicia-", email:"Alicia@fake.com"},
+            {name:"Erik-", email:"Erik@fake.com"},
+            {name:"Michael-", email:"Michael@fake.com"},
+            {name:"Kelly-", email:"Kelly@fake.com"},
+            {name:"Chelsea-", email:"Chelsea@fake.com"},
+            {name:"Luke-", email:"Luke@fake.com"},
+            {name:"Clare-", email:"Clare@fake.com"},
+            {name:"KC-", email:"KC@fake.com"},
+            {name:"Kaitlin-", email:"Kaitlin@fake.com"},
+            {name:"Michelle-", email:"Michelle@fake.com"},
+            {name:"Tracy-", email:"Tracy@fake.com"},
+            {name:"Aaron-", email:"Aaron@fake.com"},
+            {name:"Terry-", email:"Terry@fake.com"},
+            {name:"Vince-", email:"Vince@fake.com"},
+            {name:"Rom-", email:"Rom@fake.com"},
+            {name:"Steve-", email:"Steve@fake.com"},
+            {name:"Jeanne-", email:"Jeanne@fake.com"}
+        ]
+
+}
     ]);
+//
+//
+//$scope.luke = function() {
+//    $scope.name = "luke";
+//    $scope.email = "luke.k.poppe@gmail.com";
+//
+//};
+
+
 //    $scope.heading = "Message: ";
 //
 //    $scope.updateMessage = function(){
